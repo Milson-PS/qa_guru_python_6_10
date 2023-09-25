cee0515e490f9d2d2566066b21afefe27f001225
Pavel Milyukov <milson7017@yandex.ru>
2023-09-25 10:11:07 +0300

    1

--- tests/test_demoqa_automation_practice_form.py
+++ tests/test_demoqa_automation_practice_form.py
@@ -1,25 +1,26 @@
+import datetime
+
+from qa_guru_6_10.data.users import User
 from qa_guru_6_10.pages.registration_page import RegistrationPage
 
 
 def test_hard(browser_size):
-    # fill form
     registration_page = RegistrationPage()
+    student = User(
+        first_name='Ivan',
+        last_name='Ivanov',
+        email='ivan@yandex.ru',
+        gender='Male',
+        phone_number='1234567890',
+        date_of_birth=datetime.date(1992, 1, 5),
+        subjects='Economics',
+        hobbies='Sports',
+        photo='cat.jpg',
+        address='godovikova 9',
+        state='NCR',
+        city='Delhi'
+    )
     registration_page.open_browser()
-    registration_page.fill_first_name('Ivan')
-    registration_page.fill_second_name('Ivanov')
-    registration_page.fill_email('ivan@yandex.ru')
-    registration_page.choise_gender('Male')
-    registration_page.fill_phone_number('1234567890')
-    registration_page.fill_date_of_birth('1992', 'January', '05')
-    registration_page.choise_subjects('economics')
-    registration_page.choise_hobbies('Sports')
-    registration_page.upload_photo('cat.jpg')
-    registration_page.fill_address('godovikova 9')
-    registration_page.choise_state('NCR')
-    registration_page.choise_city('Delhi')
+    registration_page.fill_all(student)
     registration_page.submit()
-
-    # check form
-    registration_page.should_registered_user_with('Ivan Ivanov', 'iavn@yandex.ru', 'Male', '1234567890',
-                                                  '05 January,1992', 'Economics', 'Sports', 'cat.jpg', 'godovikova 9',
-                                                  'NCR Delhi')
+    registration_page.should_have_submited(student)
